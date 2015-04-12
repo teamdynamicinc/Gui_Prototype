4d6fec5751352680048f534742726be309cf462b
David <David@192.168.2.154>
2015-04-11 21:05:00 -0400

    added tab for results table viewer and associated buttons check TODO for more information

--- IgpGUI/src/IgpGui.java
+++ IgpGUI/src/IgpGui.java
@@ -30,14 +30,17 @@ import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.table.DefaultTableModel;
 
 import com.mysql.jdbc.ResultSetMetaData;
-/*TODO: -error boxes if spaces are detected in text boxes
- * 		-database viewer on 2nd tab
- * 		-THREE WAYS TO GET FASTA FILE: 1 organism name + 1 strain, 1 gene ID OR 1 upload 
+/*TODO: 
+ * 		IMPLEMENT CSV CONVERSION TO TABLE IN TAB3
+ * 			-have to resize the jscrolltable window
+ * 		IMPLEMENT VIEWER TO TAB 4
+ *
  */
 
 
 public class IgpGui extends JTabbedPane implements ActionListener {
-
+	
+	//primer generator tab
 	JComboBox<String> organism;		//drop down box containing organism names
 	JTextField strain;				//text field for user to enter a strain
 	JTextField geneID;				//text field for user to enter a Gene ID
@@ -53,14 +56,22 @@ public class IgpGui extends JTabbedPane implements ActionListener {
 	JButton chooseButton;
 	JTextField chosenFile;			//displays path of chosen file
 	
+	//database viewer tab
 	JComboBox<String> dbTable;		//drop down box containing table names
 	JButton tableSelect;			//select button for dropdown box after table selection is made
-	JTable tableDisplay;			//text area for display db table contents
+	JTable dbDisplay;			//text area for display db table contents
+	
+	//results table tab
+	JButton resultSubmit;
+	JButton mostRecentSubmit;
+	JButton chooseResultsButton;
+	JFileChooser chooseResultFile;
+	JTextField chosenResultFile;
+	JTable resultDisplay;
 	
 	private static final long serialVersionUID = -420254383943138649L;
 	
 	public IgpGui() throws ClassNotFoundException {			//default constructor for the GUI
-		GridBagConstraints c = new GridBagConstraints();
 		setPreferredSize(new Dimension(800, 400));
 		
 		//////////////////////////////////////////////////////////////////////////////////////////////////////*******************************************
@@ -68,19 +79,18 @@ public class IgpGui extends JTabbedPane implements ActionListener {
 		//////////////////////////////////////////////////////////////////////////////////////////////////////
 		JPanel tab1Contents = new JPanel(new GridBagLayout());			//create a panel for the tabbed pane of data entry items
 		this.add("Primer Generation", tab1Contents);							//add panel to tabbed pane
-
-		
+		GridBagConstraints tab1Constraints = new GridBagConstraints();
 		///////////////////////////
 		//ORGANISM DROP DOWN MENU//
 		///////////////////////////
 		//set up position of the organism drop down menu and its label
-		c.gridx = 0;
-		c.gridy = 0;
-		c.anchor=GridBagConstraints.WEST;
-		tab1Contents.add(new Label("Select an organism:  "), c);	//organism drop down menu label (0,0)
+		tab1Constraints.gridx = 0;
+		tab1Constraints.gridy = 0;
+		tab1Constraints.anchor=GridBagConstraints.WEST;
+		tab1Contents.add(new Label("Select an organism:  "), tab1Constraints);	//organism drop down menu label (0,0)
 		organism = new JComboBox<String>();
-		c.gridx = 1;
-		c.gridy = 0;
+		tab1Constraints.gridx = 1;
+		tab1Constraints.gridy = 0;
 		
 		//connect to db for list of organisms
 		Class.forName("com.mysql.jdbc.Driver"); //use class loader for db connections
@@ -115,43 +125,43 @@ public class IgpGui extends JTabbedPane implements ActionListener {
 				e.printStackTrace();
 			}
 		}
-		tab1Contents.add(organism, c);
+		tab1Contents.add(organism, tab1Constraints);
 		
 		
 		/////////////////////
 		//Strain Text Field//
 		/////////////////////
-		c.gridx = 2;
-		c.gridy = 0;
-		tab1Contents.add(new Label("and strain:"), c); //strain text field label (2, 0)
-		c.gridx = 3;
-		c.gridy = 0;
+		tab1Constraints.gridx = 2;
+		tab1Constraints.gridy = 0;
+		tab1Contents.add(new Label("and strain:"), tab1Constraints); //strain text field label (2, 0)
+		tab1Constraints.gridx = 3;
+		tab1Constraints.gridy = 0;
 		strain = new JTextField("", 8);
-		tab1Contents.add(strain, c);	//strain text field (3,0)
+		tab1Contents.add(strain, tab1Constraints);	//strain text field (3,0)
 		
 		
 		/////////////////////
 		//GeneID Text Field//
 		/////////////////////
-		c.gridx = 2;
-		c.gridy = 1;
-		tab1Contents.add(new Label("or gene ID:"), c); //gene id text field label (2,1)
-		c.gridx = 3;
-		c.gridy = 1;
+		tab1Constraints.gridx = 2;
+		tab1Constraints.gridy = 1;
+		tab1Contents.add(new Label("or gene ID:"), tab1Constraints); //gene id text field label (2,1)
+		tab1Constraints.gridx = 3;
+		tab1Constraints.gridy = 1;
 		geneID = new JTextField("", 8);
-		tab1Contents.add(geneID, c);	//geneID text field (2,2)
+		tab1Contents.add(geneID, tab1Constraints);	//geneID text field (2,2)
 		
 		
 		////////////////////////
 		// THRESHOLD TEXTFIELD//
 		////////////////////////
-		c.gridx = 0;
-		c.gridy = 2;
-		tab1Contents.add(new Label("Enter a threshold value (%)"), c);	//threshold value label (0,2)
-		c.gridx = 1;
-		c.gridy = 2;
+		tab1Constraints.gridx = 0;
+		tab1Constraints.gridy = 2;
+		tab1Contents.add(new Label("Enter a threshold value (%)"), tab1Constraints);	//threshold value label (0,2)
+		tab1Constraints.gridx = 1;
+		tab1Constraints.gridy = 2;
 		thresholdValue = new JTextField("90", 8);
-		tab1Contents.add(thresholdValue, c);			//threshold value text field (1,2)
+		tab1Contents.add(thresholdValue, tab1Constraints);			//threshold value text field (1,2)
 		
 		
 		//////////////////////////////
@@ -163,13 +173,13 @@ public class IgpGui extends JTabbedPane implements ActionListener {
 		humanFilter.add(humanFilterY);
 		humanFilter.add(humanFilterN);
 		humanFilterN.setSelected(true);
-		c.gridx = 0;
-		c.gridy = 3;
-		tab1Contents.add(new Label ("Filter through human genome?"), c); //Human filter radio buttons label (0,3)
-		c.gridx = 1;
-		tab1Contents.add(humanFilterY, c);	//human filter yes button (1,3)
-		c.gridx = 2;
-		tab1Contents.add(humanFilterN, c);	//human filter no button (2, 3)
+		tab1Constraints.gridx = 0;
+		tab1Constraints.gridy = 3;
+		tab1Contents.add(new Label ("Filter through human genome?"), tab1Constraints); //Human filter radio buttons label (0,3)
+		tab1Constraints.gridx = 1;
+		tab1Contents.add(humanFilterY, tab1Constraints);	//human filter yes button (1,3)
+		tab1Constraints.gridx = 2;
+		tab1Contents.add(humanFilterN, tab1Constraints);	//human filter no button (2, 3)
 		
 		//RUN HMRGD BLAST
 		hmrgdBlastY = new JRadioButton("YES");
@@ -178,13 +188,13 @@ public class IgpGui extends JTabbedPane implements ActionListener {
 		hmrgdBlast.add(hmrgdBlastY);
 		hmrgdBlast.add(hmrgdBlastN);
 		hmrgdBlastN.setSelected(true);
-		c.gridx = 0;
-		c.gridy = 4;
-		tab1Contents.add(new Label ("Run BLAST against HMRGD gut database?"), c); //Human filter radio buttons label (0,3)
-		c.gridx = 1;
-		tab1Contents.add(hmrgdBlastY, c);	//human filter yes button (1,3)
-		c.gridx = 2;
-		tab1Contents.add(hmrgdBlastN, c);	//human filter no button (2, 3)
+		tab1Constraints.gridx = 0;
+		tab1Constraints.gridy = 4;
+		tab1Contents.add(new Label ("Run BLAST against HMRGD gut database?"), tab1Constraints); //Human filter radio buttons label (0,3)
+		tab1Constraints.gridx = 1;
+		tab1Contents.add(hmrgdBlastY, tab1Constraints);	//human filter yes button (1,3)
+		tab1Constraints.gridx = 2;
+		tab1Contents.add(hmrgdBlastN, tab1Constraints);	//human filter no button (2, 3)
 		
 		//RUN LOCAL BLAST  // CHECK FOR PRIMER DIMERS CHECKBOX
 		localBlastY = new JRadioButton("YES");
@@ -193,43 +203,43 @@ public class IgpGui extends JTabbedPane implements ActionListener {
 		localBlast.add(localBlastY);
 		localBlast.add(localBlastN);
 		localBlastN.setSelected(true);
-		c.gridx = 0;
-		c.gridy = 5;
-		tab1Contents.add(new Label ("Run local BLAST against primers?"), c); //Human filter radio buttons label (0,3)
-		c.gridx = 1;
-		tab1Contents.add(localBlastY, c);	//human filter yes button (1,3)
-		c.gridx = 2;
-		tab1Contents.add(localBlastN, c);	//human filter no button (2, 3)
+		tab1Constraints.gridx = 0;
+		tab1Constraints.gridy = 5;
+		tab1Contents.add(new Label ("Run local BLAST against primers?"), tab1Constraints); //Human filter radio buttons label (0,3)
+		tab1Constraints.gridx = 1;
+		tab1Contents.add(localBlastY, tab1Constraints);	//human filter yes button (1,3)
+		tab1Constraints.gridx = 2;
+		tab1Contents.add(localBlastN, tab1Constraints);	//human filter no button (2, 3)
 		
 		
 		//////////////////////////
 		//MULTIPLEX FILE CHOOSER//
 		//////////////////////////
-		c.gridx = 0;
-		c.gridy	= 6;
-		tab1Contents.add(new Label("Upload a file for multiplex:"), c);		//file chooser label (0, 5)
-		c.gridy = 7;
-		c.fill = GridBagConstraints.BOTH;
+		tab1Constraints.gridx = 0;
+		tab1Constraints.gridy	= 6;
+		tab1Contents.add(new Label("Upload a file for multiplex:"), tab1Constraints);		//file chooser label (0, 5)
+		tab1Constraints.gridy = 7;
+		tab1Constraints.fill = GridBagConstraints.BOTH;
 		chooseFile = new JFileChooser();
 		chooseFile.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);	//set file chooser to look at files and directories
 		chooseButton = new JButton("Browse");
 		chooseButton.addActionListener(this);
 		chooseButton.setActionCommand("browse");
-		tab1Contents.add(chooseButton, c);									//file chooser button (0,6)
-		c.gridx = 1;
+		tab1Contents.add(chooseButton, tab1Constraints);									//file chooser button (0,6)
+		tab1Constraints.gridx = 1;
 		chosenFile = new JTextField(30);
-		tab1Contents.add(chosenFile, c);										//chosen file text field (1, 6)
+		tab1Contents.add(chosenFile, tab1Constraints);										//chosen file text field (1, 6)
 		
 		
 		//////////////////
 		// SUBMIT BUTTON//
 		//////////////////
-		c.gridx = 0;
-		c.gridy = 8;
-		c.fill = GridBagConstraints.BOTH;
-		c.anchor=GridBagConstraints.CENTER;
+		tab1Constraints.gridx = 0;
+		tab1Constraints.gridy = 8;
+		tab1Constraints.fill = GridBagConstraints.BOTH;
+		tab1Constraints.anchor=GridBagConstraints.CENTER;
 		submit = new JButton("Submit");
-		tab1Contents.add(submit, c);
+		tab1Contents.add(submit, tab1Constraints);
 		submit.addActionListener(this);
 		submit.setActionCommand("submit");
 	
@@ -238,16 +248,17 @@ public class IgpGui extends JTabbedPane implements ActionListener {
 		//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 		JPanel tab2Contents = new JPanel(new GridBagLayout());
 		this.add("Database Viewer", tab2Contents);
-		
+		GridBagConstraints tab2Constraints = new GridBagConstraints();
+		tab2Constraints.fill = GridBagConstraints.BOTH;
 
 		////////////////////////////
 		//TABLE SELECTION COMBOBOX//
 		////////////////////////////
-		c.gridx = 0;
-		c.gridy = 0;
-		c.weightx = 1;
-		tab2Contents.add(new Label("Select a table for viewing:"), c);
-		c.gridx = 1;
+		tab2Constraints.gridx = 0;
+		tab2Constraints.gridy = 0;
+		tab2Constraints.weightx = 1;
+		tab2Contents.add(new Label("Select a table for viewing:"), tab2Constraints);
+		tab2Constraints.gridx = 1;
 		dbTable = new JComboBox<String>();
 		//use db connection to generate radio buttons for tables
 		String getTables = "SELECT * FROM information_schema.tables WHERE TABLE_SCHEMA = 'bif712_143a03';";
@@ -262,14 +273,14 @@ public class IgpGui extends JTabbedPane implements ActionListener {
 				e.printStackTrace();
 			}
 		}
-		tab2Contents.add(dbTable, c);
+		tab2Contents.add(dbTable, tab2Constraints);
 		
 		///////////////////////////
 		//TABLE SELECTION CONFIRM//
 		///////////////////////////
-		c.gridx = 2;
+		tab2Constraints.gridx = 2;
 		tableSelect = new JButton("Select Table");
-		tab2Contents.add(tableSelect, c);
+		tab2Contents.add(tableSelect, tab2Constraints);
 		tableSelect.addActionListener(this);
 		tableSelect.setActionCommand("tableSelect");
 		
@@ -277,24 +288,68 @@ public class IgpGui extends JTabbedPane implements ActionListener {
 		////////////////////////
 		//TABLE DISPLAY WINDOW//
 		////////////////////////
-		c.gridx = 0;
-		c.gridy = 1;
-		c.fill = GridBagConstraints.BOTH;
-		c.gridwidth = 3;
-		c.weighty = 1;
-		c.weightx = 2;
-		tableDisplay = new JTable();
-		JScrollPane tableScroller = new JScrollPane(tableDisplay);
-		tab2Contents.add(tableScroller, c);
+		tab2Constraints.gridx = 0;
+		tab2Constraints.gridy = 1;
+		tab2Constraints.fill = GridBagConstraints.BOTH;
+		tab2Constraints.gridwidth = 3;
+		tab2Constraints.weighty = 1;
+		tab2Constraints.weightx = 2;
+		dbDisplay = new JTable();
+		JScrollPane tableScroller = new JScrollPane(dbDisplay);
+		tab2Contents.add(tableScroller, tab2Constraints);
+		
+		///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+		//								RESULT SET TABLE TAB								//
+		//////////////////////////////////////////////////////////////////////////////////////
+		JPanel tab3Contents = new JPanel(new GridBagLayout());
+		this.add("Results Table", tab3Contents);
+		GridBagConstraints tab3Constraints = new GridBagConstraints();
+		tab3Constraints.fill = GridBagConstraints.BOTH;
+		
+		//Results file chooser and its button
+		tab3Constraints.gridx = 0;
+		tab3Constraints.gridy = 0;
+		chooseResultFile = new JFileChooser();
+		chooseResultFile.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+		chooseResultsButton = new JButton("Browse Existing Results");
+		chooseResultsButton.addActionListener(this);
+		chooseResultsButton.setActionCommand("browseResults");
+		tab3Contents.add(chooseResultsButton, tab3Constraints);
+		
+		//Chosen file text field
+		tab3Constraints.gridx = 1;
+		chosenResultFile = new JTextField(30);
+		tab3Contents.add(chosenResultFile, tab3Constraints);
+		
+		//Result file submit button
+		tab3Constraints.gridx = 2;
+		resultSubmit = new JButton("Submit");
+		resultSubmit.addActionListener(this);
+		resultSubmit.setActionCommand("submitResultFile");
+		tab3Contents.add(resultSubmit, tab3Constraints);
+		
+		//Latest Generated File Button
+		tab3Constraints.gridx = 3;
+		mostRecentSubmit = new JButton("Use Latest Generated File");
+		mostRecentSubmit.addActionListener(this);
+		mostRecentSubmit.setActionCommand("submitResultFile");
+		tab3Contents.add(mostRecentSubmit, tab3Constraints);
+		
+		//results table and scrollpane 
+//		tab3Constraints.gridx = 0;
+//		tab3Constraints.gridy = 1;
+//		resultDisplay = new JTable();
+//		JScrollPane resultScroller = new JScrollPane(resultDisplay);
+//		tab3Contents.add(resultScroller, tab2Constraints);
 	}
 	
-	//////////////////////////////////////////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	// ACTION LISTENER CARRYS OUT ACTIONS WHEN A BUTTON IS PRESSED						//
 	//////////////////////////////////////////////////////////////////////////////////////
 	@Override
 	public void actionPerformed(ActionEvent event) {
 
-		//connect to database
+		//connect to database (PERFORMED EVERY TIME AN ACTION EVENT OCCURS)
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 		} catch (ClassNotFoundException e1) {
@@ -519,7 +574,7 @@ public class IgpGui extends JTabbedPane implements ActionListener {
 					
 					// CREATE A TABLE MODEL WITH THE DATABASE DATA WITHIN IT (by: IAndreev93 source: http://www.codeproject.com/Tips/882355/Java-and-MySQL-via-JDBC-How-to-Connect-DB-Get-Data)
 					//for changing column and row model
-				    DefaultTableModel tm = (DefaultTableModel) tableDisplay.getModel();
+				    DefaultTableModel tm = (DefaultTableModel) dbDisplay.getModel();
 				    
 				    //clear existing columns 
 				    tm.setColumnCount(0);
@@ -550,6 +605,16 @@ public class IgpGui extends JTabbedPane implements ActionListener {
 				}
 			}
 		}
+		
+		if (event.getActionCommand() == "browseResults"){				//if "browse" button is clicked, open up file choosing box
+			int returnVal = chooseResultFile.showOpenDialog(IgpGui.this);
+			
+			if (returnVal == JFileChooser.APPROVE_OPTION){				//displays file path in chosenFile text field
+				File file = chooseResultFile.getSelectedFile();
+				chosenResultFile.setText(file.getAbsolutePath());
+			}
+		}
+		
 	}
 	
 	
